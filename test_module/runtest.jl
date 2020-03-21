c0b79fbbe37a1fa6fd197b87371a5ba32fc06f12
stelong <stefano.longobardi.8@gmail.com>
2020-03-21 10:31:11 +0000

    minor

--- test_module/runtest.jl
+++ test_module/runtest.jl
@@ -8,10 +8,10 @@ using Revise
 using Solver
 
 ## Initialize simulation options
-rat = "sham" # possible rat phenotype choices are "sham" and "ab"
+rat = "ab" # possible rat phenotype choices are "sham" and "ab"
 freq = 6 # possible Hz choices are 1 and 6
-n_beats = 10000 # number of cardiac cycles to simulate
-n_curves = 10 # number of limit cardiac cycles to observe
+n_beats = 1000 # number of cardiac cycles to simulate
+n_curves = 1 # number of limit cardiac cycles to observe
 soltype = "full" # if set to "fragmentary", the solution will be saved only at stim_period-spaced time points
 
 ## Run Gattoni EP model unperturbed
@@ -29,14 +29,14 @@ perturb = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 t, sol = solveEp(rat, freq, n_beats, n_curves, soltype, perturb)
 
 ## Plot action potential and intracellular calcium concentration over time
-# p1 = plot(t, sol[13, :], label="AP")
-# p2 = plot(t, 1e3*sol[9, :], label="Ca2+")
-# plot(p1, p2, layout=(2, 1))
+p1 = plot(t, sol[13, :], label="AP")
+p2 = plot(t, 1e3*sol[9, :], label="Ca2+")
+plot(p1, p2, layout=(2, 1))
 
 ## Save time and calcium transient vectors
-M = [t 1e3*sol[9, :]] # two-column matrix
-mypath = "/home/sl18/Desktop/" # where to save
-filename = rat*"_"*string(freq)*"_"*"Ca_transient.txt"
-open(mypath*filename, "w") do f
-	writedlm(f, M)
-end
+# M = [t 1e3*sol[9, :]] # two-column matrix
+# mypath = "/home/sl18/Desktop/" # where to save
+# filename = rat*"_"*string(freq)*"_"*"Ca_transient.txt"
+# open(mypath*filename, "w") do f
+# 	writedlm(f, M)
+# end
